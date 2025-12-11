7b680f8cc42bfe6385787740347b3f77a02ff18f
arnausanchez-svg <arnau.sanchez@alumne.escolabalmes.com>
2025-12-11 16:31:16 +0100

    Añadida fecha al script de diagnóstico de disco

--- check-disk.ps1
+++ check-disk.ps1
@@ -1,1 +1,4 @@
-Get-Volume | Select-Object DriveLetter, FileSystem, Write-Output "Revisión del disco completada correctamente."
+$fecha = Get-Date
+Write-Output "Análisis realizado el: $fecha"
+Get-Volume | Select-Object DriveLetter, FileSystem, SizeRemaining, Size
+Write-Output "Revisión del disco completada correctamente."
